eb43541519ffdc9981006c2f3370d42ef2e9d8da
raouia ben nasib <raouia.ben19@gmail.com>
2020-11-14 12:50:50 +0100

    1 ere partie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './compsant/accueil/accueil.component';
+import { ListmaterielComponent } from './compsant/informatique/listmateriel/listmateriel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ListmaterielComponent
   ],
   imports: [
     BrowserModule
