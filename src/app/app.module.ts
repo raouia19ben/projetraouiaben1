192a722dfb3e21e9d9e902d398f8904d681444f0
raouia ben nasib <raouia.ben19@gmail.com>
2020-12-02 17:24:56 +0100

    detailmateriel mis en forme »

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { AccueilComponent } from './compsant/accueil/accueil.component';
 import { ListmaterielComponent } from './compsant/informatique/listmateriel/listmateriel.component';
 import { MaterielComponent } from './compsant/informatique/materiel/materiel.component';
-import { DetailmaterielComponent } from './informatique/detailmateriel/detailmateriel.component';
+import { DetailmaterielComponent } from './compsant/informatique/detailmateriel/detailmateriel.component';
 
 @NgModule({
   declarations: [
